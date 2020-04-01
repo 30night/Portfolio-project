a3f01a132a1cf8b18d6f4afd250646db9bb3a7f3
30night <haroscmil2011@gmail.com>
2020-04-01 11:48:44 +1100

    added line to the typewriter

--- main.js
+++ main.js
@@ -9,6 +9,51 @@ class TypeWriter {
     this.isDeleting = false;
   }
 
+  type() {
+
+    const current = this.wordIndex % this.words.length;
+ 
+    const fullTxt = this.words[current];
+
+
+    if(this.isDeleting) {
+
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    
+    if(!this.isDeleting && this.txt === fullTxt) {
+      
+      typeSpeed = this.wait;
+      
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+     
+      this.wordIndex++;
+     
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
